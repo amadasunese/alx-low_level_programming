5425123c525395ee23994e4784b3ed888cc7cdf4
Ese Amadasun <amadasunese@gmail.com>
2022-12-12 10:54:21 -0800

    Inventing is a combination of brains and materials.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,17 @@
 */
 int main(void)
 {
-  int tens;
-  int ones;
+int tens;
+int ones;
 
-  for (tens = 0; tens <= 9; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
+for (tens = 0; tens <= 9; tens++)
 	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+	for (ones = tens + 1; ones <= 9; ones++)
+	{
+	putchar(tens + '0');
+	putchar(ones + '0');
 
-	  if (tens < 8)
+	if (tens < 8)
 	    {
 	      putchar(',');
 	      putchar(' ');
