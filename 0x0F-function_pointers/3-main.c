f25903c7a73019af3bf83c4b90908c6518888d05
Ese Amadasun <amadasunese@gmail.com>
2023-01-20 09:30:38 -0800

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,55 +1,36 @@
-#include <stdio.h>
-#include "function_pointers.h"
-
+#include "3-calc.h"
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_98(int elem)
+ * main - Entry point
+ * @argc: the number of the parameters
+ * @argv: the parameters in the case the number to be calculated.
+(* a blank line
+* Description: this program is the enttry point for a calculator)?
+(* section header: 3-calc.h)*
+* Return: 0 in success
+*/
+int main(int argc, char *argv[])
 {
-  return (98 == elem);
-}
+	int n1, n2, result;
+	int (*p)(int, int);
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-  return (elem > 0);
-}
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-  return (elem == 98 || -elem == 98);
-}
+	p = get_op_func(argv[2]);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-  int index;
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = p(n1, n2);
 
-  index = int_index(array, 20, is_98);
-  printf("%d\n", index);
-  index = int_index(array, 20, abs_is_98);
-  printf("%d\n", index);
-  index = int_index(array, 20, is_strictly_positive);
-  printf("%d\n", index);
-  return (0);
+	printf("%d\n", result);
+	return (0);
 }
